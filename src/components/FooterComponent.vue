68f4066e23cc109576ea08d8c604361a3eefefce
eldin001 <eldin.balje@icloud.com>
2024-04-11 16:19:32 +0200

    added big logo

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        
-    </div>
+    <footer>
+        <div class="row">
+            <div class="col-6">
+
+            </div>
+            <div class="col-6">
+                <img src="/img/dc-logo-bg.png" alt="">
+            </div>
+        </div>
+     
+    </footer>
 </template>
 
 <script>
@@ -11,5 +19,8 @@
 </script>
 
 <style lang="scss" scoped>
+footer {
+    background-image: url(../../public/img/footer-bg.jpg);
+}
 
 </style>
