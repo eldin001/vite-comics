2b0770605b707a7904767fb03eb6d825e41a85e1
eldin001 <eldin.balje@icloud.com>
2024-04-11 14:08:27 +0200

    added components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,68 @@
+<template>
+    <header class="bg-white">
+        <div class="container d-flex justify-content-between mt-3 ">
+            <img src="/img/dc-logo.png" alt="Logo DC comics">
+            <ul class="d-flex align-items-center list-unstyled gap-3">
+                <li v-for="(item, index) in menu" :key="index">
+                    <a class="text-decoration-none text-uppercase text-secondary " :href="item.url">{{ item.title }}</a>
+                </li>
+            </ul>
+
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            menu: [
+                {
+                    title: "characters",
+                    url: "#"
+                },
+                {
+                    title: "comics",
+                    url: "#"
+                },
+                {
+                    title: "movies",
+                    url: "#"
+                },
+                {
+                    title: "tv",
+                    url: "#"
+                },
+                {
+                    title: "games",
+                    url: "#"
+                },
+                {
+                    title: "collectibles",
+                    url: "#"
+                },
+                {
+                    title: "videos",
+                    url: "#"
+                },
+                {
+                    title: "fans",
+                    url: "#"
+                },
+                {
+                    title: "news",
+                    url: "#"
+                },
+                {
+                    title: "shop",
+                    url: "#"
+                },
+            ],
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Main</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
